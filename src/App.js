93d6e8614680b36b551afca2306a19d0cdeb777f
johnMorone <jmorone@alumni.princeton.edu>
2021-04-02 03:28:25 -0700

    Only Planes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 import {Post} from "./post";
 
 function App() {
 
-    const shibariMessage = <div><span>Sorry babe, I'm all </span><a href="https://www.youtube.com/channel/UCXuqSBlHAE6Xw-yeJA0Tunw">#tiedUp</a></div>
-    const stickMessage = <div><span>My stick is hard</span><i> and </i><span>I'm excited to see you</span></div>
-    const gimpMessage = <div><a href="https://www.youtube.com/channel/UCXuqSBlHAE6Xw-yeJA0Tunw">Sub2LTT</a></div>
+    const shibariMessage = <div><span>Sorry babe, I'm all </span><a href="https://www.youtube.com/channel/UCXuqSBlHAE6Xw-yeJA0Tunw" target="_blank">#tiedUp</a></div>
+    const stickMessage = <div><span>My flight stick is hard</span><i> and </i><span>I'm excited to see you</span></div>
+    const gimpMessage = <div><a href="https://www.youtube.com/channel/UCXuqSBlHAE6Xw-yeJA0Tunw" target="_blank">Sub2LTT</a></div>
 
     const lttVid = <div className="video-responsive">
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/dJwjqZZgcWk"
+        <iframe width="470" height="220" src="https://www.youtube.com/embed/dJwjqZZgcWk"
                 title="YouTube video player"
                 frameBorder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
@@ -20,9 +19,9 @@ function App() {
     return (
     <div className="App">
         <div className="header"><img src="/logo.png" /></div>
-      <Post message="Announcement vid <3" numLikes={69} timePosted={2}>{lttVid}</Post>
-      <Post message={shibariMessage} imageSrc="/shibari.png" numLikes={69} timePosted={2} />
-      <Post message={stickMessage} imageSrc="/stick.jpg" numLikes={13.3} timePosted={10} />
+      <Post message="Announcement vid <3" numLikes={169000000} timePosted={35}>{lttVid}</Post>
+      <Post message={shibariMessage} imageSrc="/shibari.png" numLikes={69} timePosted={49} />
+      <Post message={stickMessage} imageSrc="/stick.jpg" numLikes={13.3} timePosted={58} />
       <Post message={gimpMessage} imageSrc="/gimp.jpg" numLikes={420} timePosted={69} />
     </div>
     );
--- src/post.tsx
+++ src/post.tsx
@@ -18,21 +18,19 @@ export function Post(props: PostProps) {
     <div className="tw-block-parent">
       <div className="timeline-PostList-post">
         <div className="timeline-Post">
-          <div className="timeline-Post-brand">
-            <div className="Icon Icon--post"><img src="/profilepic.png" /></div>
-          </div>
           <div className="timeline-Post-author">
-            <div className="PostAuthor"><a className="PostAuthor-link" href="#channel"> </a><span
-              className="PostAuthor-avatar">
-            <div className="Avatar"> </div></span><span className="PostAuthor-name">6900XT </span><span
+            <div className="PostAuthor"><a className="PostAuthor-link" href="#channel"> </a><img className="PostAuthor-avatar" src="/profile192.png" />
+            <span className="PostAuthor-name">6900XT </span><span
               className="Icon Icon--verified"> </span><span className="PostAuthor-screenName">@6900XT </span></div>
           </div>
-          {children}
-          <div className="timeline-Post-image"><img src={imageSrc}/></div>
-          <div className="timeline-Post-text">{message}</div>
+          <div className="timeline-Post-content">
+            {children}
+            {imageSrc && <a className="timeline-Post-image" href={imageSrc} target="_blank"><img src={imageSrc}/></a>}
+            <div className="timeline-Post-text">{message}</div>
+          </div>
           <div className="timeline-Post-metadata"><span className="timeline-Post-timestamp">{timePosted}h</span></div>
           <ul className="timeline-Post-actions">
-            <li className="timeline-Post-action"><a className="Icon Icon--heart" href="#">{numLikes}</a></li>
+            <li className="timeline-Post-action"><a className="Icon Icon--heart" href="#"></a>{numLikes}</li>
             <li className="timeline-Post-action"><a className="Icon Icon--share" href="#"></a></li>
           </ul>
         </div>
